6693244de823a323008e9918b5e23c9db3fb0c71
Sweta Kumavat <74348944+Swetamevada@users.noreply.github.com>
2024-01-11 21:53:59 -0500

    Update auth_users.js

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -9,10 +9,6 @@ const isValid = (username)=>{ //returns boolean
 //write code to check is the username is valid
 }
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
-}
-
 //only registered users can login
 regd_users.post("/login", (req,res) => {
   //Write your code here
@@ -20,11 +16,6 @@ regd_users.post("/login", (req,res) => {
 });
 
 // Add a book review
-regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
-
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
 module.exports.users = users;
